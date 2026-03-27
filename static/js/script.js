1d6a80b1e427c9e3c53614b6f1f68e1a375fdc36
Vishal Kumar Phadwa <computersciencestudent2203@gmail.com>
2026-03-27 11:45:28 +0530

    Some

--- static/js/script.js
+++ static/js/script.js
@@ -301,17 +301,3 @@ function checkMatch() {
             formToSubmit.submit();
         }
     });
-
-// Lightbox Logic For Image Section
-
-    function openLightbox(imgSrc) {
-        document.getElementById('lightboxImg').src = imgSrc;
-        document.getElementById('imageLightbox').classList.add('active');
-    }
-    
-    function closeLightbox(event) {
-        // Only close if clicking the background or the 'X' button
-        if(event.target.id === 'imageLightbox' || event.target.closest('.lightbox-close')) {
-            document.getElementById('imageLightbox').classList.remove('active');
-        }
-    }
